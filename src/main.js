93f46a2950d7faed6944bcfcb44c1470b3282463
MengYX <admin@mengyx.cn>
2020-05-18 16:40:39 +0800

    Add Tips

--- src/main.js
+++ src/main.js
@@ -2,9 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import {
-    Button, Col, Container, Footer, Icon, Image, Link, Main,
-    Row, Table, TableColumn, Upload, Radio, Checkbox, Progress,
-    Notification, Tooltip,
+    Button,
+    Checkbox,
+    Col,
+    Container,
+    Footer,
+    Icon,
+    Image,
+    Link,
+    Main,
+    Notification,
+    Progress,
+    Radio,
+    Row,
+    Table,
+    TableColumn,
+    Tooltip,
+    Upload
 } from 'element-ui';
 import 'element-ui/lib/theme-chalk/base.css';
 
@@ -27,7 +41,7 @@ Vue.use(Progress);
 Vue.prototype.$notify = Notification;
 
 Vue.config.productionTip = false;
-
+document.getElementById("loader-source").remove()
 new Vue({
     render: h => h(App),
 }).$mount('#app');
