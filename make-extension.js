8eaf46e534ab7b7551e3be6f73d6fa419f0c76f9
Emmm Monster <58943012+emmmx@users.noreply.github.com>
2021-08-11 23:04:01 +0800

    fix(extension): compute version name

--- make-extension.js
+++ make-extension.js
@@ -15,6 +15,9 @@ const manifest = JSON.parse(manifestRaw)
 const pkgRaw = fs.readFileSync("./package.json", "utf-8")
 const pkg = JSON.parse(pkgRaw)
 
-manifest["version"] = pkg["version"]
+ver_str = pkg["version"]
+if (ver_str.startsWith("v")) ver_str = ver_str.slice(1)
+manifest["version"] = ver_str
+
 fs.writeFileSync("./dist/manifest.json", JSON.stringify(manifest), "utf-8")
 console.log("Write: manifest.json")
